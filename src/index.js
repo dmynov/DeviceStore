feb2906298127222452c8c675010c7ef4de4844b
dmynov <74049804+dmynov@users.noreply.github.com>
2021-12-23 14:40:15 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import UserStore from "./store/UserStore";
+import DeviceStore from "./store/DeviceStore";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export const Context = createContext(null)
+
+ReactDOM.render(
+    <Context.Provider value={{
+        user: new UserStore(),
+        device: new DeviceStore(),
+    }}>
+        <App />
+    </Context.Provider>,
+  document.getElementById('root')
+);
+
